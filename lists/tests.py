c384392617ed0591108d78cb17210cc6ddae9d26
Ali Payne <payneal@onid.oregonstate.edu>
2015-09-28 20:49:36 -0400

    passed functional test To-Do in browser title

--- lists/tests.py
+++ lists/tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
-#from django.http import HttpRequest
+from django.http import HttpRequest
 
-#from lists.views import home_page
+from lists.views import home_page
 
 # Create your tests here.
 
 class HomePageViewTest(TestCase): 
 
 	def test_home_page_returns_correct_html(self):
-		self.assertEqual(1+1, 3)
-		#request = HttpRequest()
-		#response = home_page(request)
-		#self.assertIn('<title> To-Do lists </title>', response.content)
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertIn('<title>To-Do lists</title>', response.content.decode('UTF-8'))
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertTrue(response.content.endswith(b'</html>'))
 		
